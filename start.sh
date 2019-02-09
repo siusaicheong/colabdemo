ad2caf7b71d02e58efe7cd1b9302ddced26c189c
siusaicheong <siusaicheong@gmail.com>
2019-02-10 02:07:15 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'Hello. Loading the program'
 
-wget -O /nbextensions/google.colab/example12345678.jpg https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Thomas_Cook_Dublin_protest.jpg/120px-Thomas_Cook_Dublin_protest.jpg
-#wget -O /usr/local/share/jupyter/nbextensions/google.colab/example12345678.jpg https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Thomas_Cook_Dublin_protest.jpg/120px-Thomas_Cook_Dublin_protest.jpg
+#wget -O /nbextensions/google.colab/example12345678.jpg https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Thomas_Cook_Dublin_protest.jpg/120px-Thomas_Cook_Dublin_protest.jpg
+wget -O /usr/local/share/jupyter/nbextensions/google.colab/example12345678.jpg https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Thomas_Cook_Dublin_protest.jpg/120px-Thomas_Cook_Dublin_protest.jpg
