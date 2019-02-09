4751ee83f9277ad673894b3e23624759aea938ee
siusaicheong <siusaicheong@gmail.com>
2019-02-10 00:55:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo 'Hello. Loading the program'
 wget -O /usr/local/share/jupyter/nbextensions/google.colab/example12345678.jpg https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Thomas_Cook_Dublin_protest.jpg/120px-Thomas_Cook_Dublin_protest.jpg
+python start.py
